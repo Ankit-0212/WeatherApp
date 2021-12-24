e897e293c55e9e4767637056a213057a8d4c3101
Ankit-0212 <ankitrajsrivastava222@gmail.com>
2021-12-24 08:36:05 +0530

    fixed icon issue

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import CityComponent from "./modules/CityComponent";
 import WeatherComponent from "./modules/WeatherInfoComponent";
 
 export const WeatherIcons = {
-  "01d": "/icons/sunny.svg",
-  "01n": "/icons/night.svg",
-  "02d": "/icons/day.svg",
-  "02n": "/icons/cloudy-night.svg",
-  "03d": "/icons/cloudy.svg",
-  "03n": "/icons/cloudy.svg",
-  "04d": "/icons/perfect-day.svg",
-  "04n": "/icons/cloudy-night.svg",
-  "09d": "/icons/rain.svg",
-  "09n": "/icons/rain-night.svg",
-  "10d": "/icons/rain.svg",
-  "10n": "/icons/rain-night.svg",
-  "11d": "/icons/storm.svg",
-  "11n": "/icons/storm.svg",
+  "01d": "weatherapp/icons/sunny.svg",
+  "01n": "weatherapp/icons/night.svg",
+  "02d": "weatherapp/icons/day.svg",
+  "02n": "weatherapp/icons/cloudy-night.svg",
+  "03d": "weatherapp/icons/cloudy.svg",
+  "03n": "weatherapp/icons/cloudy.svg",
+  "04d": "weatherapp/icons/perfect-day.svg",
+  "04n": "weatherapp/icons/cloudy-night.svg",
+  "09d": "weatherapp/icons/rain.svg",
+  "09n": "weatherapp/icons/rain-night.svg",
+  "10d": "weatherapp/icons/rain.svg",
+  "10n": "weatherapp/icons/rain-night.svg",
+  "11d": "weatherapp/icons/storm.svg",
+  "11n": "weatherapp/icons/storm.svg",
 };
 
 const Container = styled.div`
